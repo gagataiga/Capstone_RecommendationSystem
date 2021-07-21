8713e6d55b4d7d357d4932c2880f92546f5234ea
志賀大河 <gaitatg97@gmail.com>
2021-07-21 15:51:50 +0900

    add directorAndMovie

--- FirstRatings.java
+++ FirstRatings.java
@@ -25,16 +25,52 @@ public class FirstRatings {
         return movieList;
     }
     
+	public HashMap<String, Integer> directorAndMovie(ArrayList<Movie> movies) {
+		HashMap<String, Integer> directoresMap = new HashMap<String, Integer>();
+		for (Movie movie : movies) {
+			String director = movie.getDirector();
+			// directores doesn't contain "," 
+			// and directoresMap doesn't contain the directore name
+			if (!director.contains(",") && !directoresMap.containsKey(director)){
+				// put
+				directoresMap.put(director, 1);
+		    // directores doesn't contain ","
+			// but directoresMap contains dire
+			} else if (!director.contains(",")) {
+			// increment key value
+				directoresMap.put(director, directoresMap.get(director) + 1);
+			}
+			
+			// director contains ","
+			if (director.contains(",")) {
+				String regex = ",";
+				// split with ",""
+				String[] data = director.split(regex);
+				// "str = directore name"
+				for (String key : data) {
+					if (directoresMap.containsKey(key)) {
+						directoresMap.put(key, directoresMap.get(key) + 1);
+					} else {
+						directoresMap.put(key, 1);
+					}
+				}
+			}
+			
+		}
+
+		return directoresMap;
+	}
+
 	public void testLoadMovies() {
 		// get movielist 
 		ArrayList<Movie> movielist = LoadMovies("ratedmoviesfull.csv");
 		// conform the number of movielist
 		// ratedmoviesfull.csv -> 3142 ok	
-		for (Movie movie : movielist) {
-			System.out.println(movie.getDirector());
-		}
+		 //for (Movie movie : movielist) {
+		// 	System.out.println(movie.getDirector());
+		 //}
 		int countComedies = 0;
-		int lt150minMovies = 0;
+		int gt150minMovies = 0;
 		// Movie comdyMovie;
 		for (Movie movie : movielist) {
 			// 1 Add code to determine how many movies include the Comedy genre. 
@@ -44,15 +80,41 @@ public class FirstRatings {
 			}
 			// Add code to determine how many movies are greater than 150 minutes in length. 
 			// In the file ratedmovies_short.csv, there are two. 
-			if (movie.getMinutes() < 150) {
-				lt150minMovies += 1;
+			if (movie.getMinutes() > 150) {
+				gt150minMovies += 1;
 			}
 		}
 		System.out.println(countComedies);
-		System.out.println(lt150minMovies);
+		System.out.println(gt150minMovies);
 
 		// 2 Add code to determine the maximum number of movies by any director, and who the directors are that directed that many movies
-		
+		HashMap<String, Integer> directorAndMovieMap = directorAndMovie(movielist);
+
+		int maximum = 0;
+		String director = "";
+		for (String key : directorAndMovieMap.keySet()) {
+			// System.out.println(key + ":" + directorAndMovieMap.get(key));
+			if (maximum == 0) {
+				maximum = directorAndMovieMap.get(key);
+				director = key;
+			} else if (maximum < directorAndMovieMap.get(key)) {
+				maximum = directorAndMovieMap.get(key);
+				director = key;
+			}
+		}
+
+		ArrayList<String> maxdirectores = new ArrayList<String>();
+		for (String key : directorAndMovieMap.keySet()) {
+			if (directorAndMovieMap.get(key) == maximum) {
+				maxdirectores.add(key);
+			}	
+		}
+
+		System.out.println("the maximum number of movies by any director" + maximum);
+		System.out.println("who the directors are that directed that many movies ");
+		for (String s : maxdirectores){
+			System.out.println(s);
+		}
 	}
 	
 
