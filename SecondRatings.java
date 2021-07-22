a86d0867ef004fcd1a26ee8983e442c39245709c
志賀大河 <gaitatg97@gmail.com>
2021-07-22 11:50:51 +0900

    Start 5-2

---
+++ SecondRatings.java
@@ -0,0 +1,26 @@
+
+/**
+ * Write a description of SecondRatings here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+
+import java.util.*;
+
+public class SecondRatings {
+    private ArrayList<Movie> myMovies;
+    private ArrayList<Rater> myRaters;
+    
+
+    public SecondRatings(String movieFile,String ) {
+        
+        
+    }
+
+    public SecondRatings() {
+        // default constructor
+        this("ratedmoviesfull.csv", "ratings.csv");
+    }
+    
+}
