c45eb819cb27e944ef99546be2a6abab3e66755d
志賀大河 <gaitatg97@gmail.com>
2021-07-22 14:38:12 +0900

    add getAverageRatings getAverageById

--- MovieRunnerAverage.java
+++ MovieRunnerAverage.java
@@ -1,3 +1,7 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Collections;
+
 
 /**
  * Write a description of MovieRunnerAverage here.
@@ -6,5 +10,29 @@
  * @version (a version number or a date)
  */
 public class MovieRunnerAverage {
+		
+
+	public void printAverageRatings() {
+		SecondRatings secondRatings = new SecondRatings();
+		System.out.println("movie size " + secondRatings.getMovieSize());
+		System.out.println("rating size " + secondRatings.getRaterSize());
+
+		ArrayList<Rating> ratingList = secondRatings.getAverageRatings(3);
+		// bubblie sort
+		for (int i = 0; i < ratingList.size() - 1; i++) {
+			for (int j = 0; j < ratingList.size() - 1; j++) {
+				Double value = ratingList.get(j).getValue();
+				Double valuePuls = ratingList.get(j + 1).getValue();
+				// 現在のvalueと次のvalueを比べてvalueがでかい場合
+				if (value > valuePuls) {
+					Collections.swap(ratingList, j, j + 1);
+				}
+			}
+		}
+
+		for (Rating rating : ratingList) {
+			System.out.println("movie: " + rating.getItem() + " rating: "+ rating.getValue() );
+		}
+	}
 
 }
--- SecondRatings.java
+++ SecondRatings.java
@@ -6,6 +6,7 @@
  * @version (a version number or a date)
  */
 
+import java.lang.reflect.Array;
 import java.util.*;
 
 public class SecondRatings {
@@ -13,14 +14,78 @@ public class SecondRatings {
     private ArrayList<Rater> myRaters;
     
 
-    public SecondRatings(String movieFile,String ) {
-        
-        
+    public SecondRatings(String movieFile,String ratingFile) {
+        FirstRatings firstRatings = new FirstRatings();
+        this.myMovies = firstRatings.LoadMovies(movieFile);
+        this.myRaters = firstRatings.loadRaters(ratingFile);
     }
 
     public SecondRatings() {
-        // default constructor
-        this("ratedmoviesfull.csv", "ratings.csv");
+        this("ratedmovies_short.csv", "ratings_short.csv");
     }
+
+    // return the number of raters that were read in and stored in the ArrayList of type Rater
+    public int getRaterSize() {
+        return this.myRaters.size();
+    }
+
+     // return the number of movies that were read in and stored in the ArrayList of type Movie
+    public int getMovieSize() {
+        return this.myMovies.size();
+    }
+
+
+    // This method returns a double representing the average movie rating for this ID if there are at least minimalRaters ratings. If there are not minimalRaters ratings, then it returns 0.0.
+    private double getAverageById(String id, int minimalRaters) {
+        double totalRating = 0;
+        double avarage = 0;
+        double countRaters = 0;
+
+        for (Rater rater : this.myRaters) {
+            for (int i = 0; i < rater.numRatings(); i++) {
+                // get itemlist
+                ArrayList<String> list = rater.getItemsRated();
+                // movie_id == id 
+                if (list.get(i).equals(id)) {
+                    double tempRatingNum = rater.getRating(list.get(i));
+                    totalRating += tempRatingNum;
+                    countRaters +=1;
+                }
+            }
+        }
+        
+        if (countRaters < minimalRaters) {
+               return 0.0;
+            } else {
+               avarage = totalRating / countRaters;
+            }
+
+        return avarage;
+    }
+
+    //an ArrayList of all the Rating objects for movies that have at least the minimal number of raters supplying a rating
+    public ArrayList<Rating> getAverageRatings(int minimalRaters) {
+        ArrayList<Rating> ratingsList = new ArrayList<Rating>();
+        for (Movie movie : myMovies) {
+            double avaRatingNum = getAverageById(movie.getID(), minimalRaters);
+            // greater than 0.0 is okey
+            if (avaRatingNum != 0.0) {
+                Rating rating = new Rating(movie.getTitle(), avaRatingNum);
+                ratingsList.add(rating);
+            }
+        }
+        return ratingsList;
+    }
+
+    // This method returns the title of the movie with that ID. If the movie ID does not exist, then this method should return a String indicating the ID was not found.
+    public String getTitle(String id) {
+        for (Movie movie : myMovies) {
+            if (movie.getID().equals(id)) {
+                return movie.getTitle();
+            }
+        }
+        return id + "was not found";
+    }
+    
     
 }
